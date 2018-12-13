52ed9e6adc53865a54f4222082f72eca7b0a6cd5
kairuspir <kairuspir@gmail.com>
2018-12-13 18:08:12 +0530

    Changed icon, added firebase plugin and added multidex support for android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "nativescript-plugin-firebase";
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html",
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        firebase.init({
+            persist: true,
+            storageBucket: "gs://zoroastrian-calendar-project.appspot.com"
+        }).then(
+            instance => {
+                console.log("firebase.init done");
+            },
+            error => {
+                console.log(`firebase.init error: ${error}`);
+            }
+        );
+
+    }
+}
